53312b14d2010bca1d46916ac341a68ee165be3e
Emmanuel <e.ngwoke@alustudent.com>
2025-07-25 11:09:20 +0200

    fix:Update session.ts file to use expo-secure-store

--- uzimaai-app/utils/session.ts
+++ uzimaai-app/utils/session.ts
@@ -1,16 +1,31 @@
+import { Platform } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SESSION_KEY = 'uzimaai_session';
 
 export async function saveSession(user: any) {
-  await SecureStore.setItemAsync(SESSION_KEY, JSON.stringify(user));
+  if (Platform.OS === 'web') {
+    await AsyncStorage.setItem(SESSION_KEY, JSON.stringify(user));
+  } else {
+    await SecureStore.setItemAsync(SESSION_KEY, JSON.stringify(user));
+  }
 }
 
 export async function getSession() {
-  const data = await SecureStore.getItemAsync(SESSION_KEY);
+  let data;
+  if (Platform.OS === 'web') {
+    data = await AsyncStorage.getItem(SESSION_KEY);
+  } else {
+    data = await SecureStore.getItemAsync(SESSION_KEY);
+  }
   return data ? JSON.parse(data) : null;
 }
 
 export async function clearSession() {
-  await SecureStore.deleteItemAsync(SESSION_KEY);
+  if (Platform.OS === 'web') {
+    await AsyncStorage.removeItem(SESSION_KEY);
+  } else {
+    await SecureStore.deleteItemAsync(SESSION_KEY);
+  }
 } 
