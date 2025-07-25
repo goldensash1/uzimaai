e352440a5e3d15b4a2ecf293bfc10a96a0068249
chiagbanweghi101 <c.peter@alustudent.com>
2025-07-25 20:48:25 +0200

    Proper doc: documented the not-found component

--- uzimaai-app/app/+not-found.tsx
+++ uzimaai-app/app/+not-found.tsx
@@ -3,13 +3,20 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 
+/**
+ * NotFoundScreen is displayed when a user navigates to a route that does not exist.
+ * It provides a message and a link to return to the home screen.
+ */
 export default function NotFoundScreen() {
   return (
     <>
+      {/* Set the screen title to 'Oops!' */}
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
+        {/* Display not found message */}
         <Text style={styles.title}>This screen doesn't exist.</Text>
 
+        {/* Link to navigate back to the home screen */}
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
@@ -18,6 +25,9 @@ export default function NotFoundScreen() {
   );
 }
 
+/**
+ * Styles for the NotFoundScreen components.
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
